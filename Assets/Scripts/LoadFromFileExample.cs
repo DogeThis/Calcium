2fb4566303099307755c353c53eb62ce8291c4b7
The Doge <alex.ren@gmail.com>
2023-06-23 21:02:10 -0700

    allow naming of output bundle

--- Assets/Scripts/LoadFromFileExample.cs
+++ Assets/Scripts/LoadFromFileExample.cs
@@ -1,19 +1,30 @@
 using System.IO;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 public class LoadFromFileExample : MonoBehaviour
 {
-    public string pathToAssetBundle = "ubody_dnc0am_c403";
+    // label as "Name of bundle" in the editor
+    
+    [Tooltip("The name of the bundle you want to load for spring bone information. No .bundle extension, we'll add it for you.")]
+    [FormerlySerializedAs("pathToAssetBundle")] public string nameOfBundleToLoad = "ubody_dnc0am_c403";
+    
     public GameObject loadedPrefab;
 
     public GameObject editedMesh;
 
     public  GameObject createdObject;
+
+    [Tooltip("The name of the bundle you want to build. (this is what you assigned yourself in Unity's editor) (no .bundle extension, we'll add it for you")]
+    public string nameOfBundleToBuild;
+
+    [Tooltip("Name of the output bundle (no .bundle extension, we'll add it for you")]
+    public string outputFileName = "output_name";
     public void LoadAssetBundle()
     {
         // remove ".bundle" from the name if it happens to be there
-        var nameToUse = pathToAssetBundle.Replace(".bundle", "") + ".bundle";
-        if (pathToAssetBundle == null) 
+        var nameToUse = nameOfBundleToLoad.Replace(".bundle", "") + ".bundle";
+        if (nameOfBundleToLoad == null) 
         {
             Debug.Log("no path to asset bundle");
             return;
--- Assets/Scripts/LoadFromFileExampleEditor.cs
+++ Assets/Scripts/LoadFromFileExampleEditor.cs
@@ -1,6 +1,8 @@
 ﻿#if UNITY_EDITOR
+using System.Collections.Generic;
 using UnityEditor;
 using UnityEngine;
+using UnityEngine.Serialization;
 
 [CustomEditor(typeof(LoadFromFileExample))]
 public class LoadFromFileExampleEditor: Editor
@@ -24,9 +26,28 @@ public class LoadFromFileExampleEditor: Editor
             myTarget.UnloadAssetBundles();
         }
 
-        if (GUILayout.Button("Build bundle"))
+        if (GUILayout.Button("Build bundle (you may have to hit me twice"))
         {
-            BuildPipeline.BuildAssetBundles ("Assets/AssetBundles", BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
+            // Argument validation
+            var assetBundleName = myTarget.nameOfBundleToBuild;
+            if (assetBundleName == null)
+            {
+                Debug.Log("I need an asset bundle name to continue.");
+                return;
+            }
+            
+            List<AssetBundleBuild> builds = new List<AssetBundleBuild>();
+
+            var assetPaths = AssetDatabase.GetAssetPathsFromAssetBundle(assetBundleName);
+
+            AssetBundleBuild build = new AssetBundleBuild();
+            build.assetBundleName = myTarget.outputFileName + ".bundle";
+            build.assetNames = assetPaths;
+
+            builds.Add(build);
+            Debug.Log("assetBundle to build:" + build.assetBundleName);
+            
+            BuildPipeline.BuildAssetBundles ("Assets/AssetBundles",builds.ToArray(), BuildAssetBundleOptions.None, EditorUserBuildSettings.activeBuildTarget);
         }
     }
 }
